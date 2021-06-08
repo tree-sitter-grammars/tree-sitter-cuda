25e18d68e2cf4fb5622d7ec757a2e72860f0a546
Stephan Seitz <stephan.seitz@fau.de>
2021-06-08 22:32:08 +0200

    Initial working commit

--- grammar.js
+++ grammar.js
@@ -7,24 +7,28 @@ module.exports = grammar(CPP, {
         $.raw_string_literal
     ],
 
-    function_definition: ($, original) => seq(
-        seq(
-            rep(
-                choice(
-                    '__device__',
-                    '__host__',
-                    '__forceinline__'
-                )
-            ),
-        ), original
-    ),
+    rules: {
+        _top_level_item: (_, original) => original,
+
+        function_definition: (_, original) => seq(
+            seq(
+                repeat(
+                    choice(
+                        '__device__',
+                        '__host__',
+                        '__forceinline__'
+                    )
+                ),
+            ), original
+        ),
 
 
-    type_qualifier: (_, original) => choice(
-        original,
-        '__shared__',
-        '__global__',
-        '__local__',
-        '__constant__',
-    ),
+        type_qualifier: (_, original) => choice(
+            original,
+            '__shared__',
+            '__global__',
+            '__local__',
+            '__constant__',
+        ),
+    }
 });
