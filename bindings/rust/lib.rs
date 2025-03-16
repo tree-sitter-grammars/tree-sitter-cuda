f8d3a20032b90232bd035448b75dc10328d54a82
Stephan Seitz <sseitz@nvidia.com>
2025-03-16 16:55:30 +0100

    bindings: use tree_sitter_language and Rust edition 2024

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -1,32 +1,32 @@
 //! This crate provides cuda language support for the [tree-sitter][] parsing library.
 //!
-//! Typically, you will use the [language][language func] function to add this language to a
+//! Typically, you will use the [LANGUAGE_CUDA][] constant to add this language to a
 //! tree-sitter [Parser][], and then use the parser to parse some code:
 //!
 //! ```
+//! use tree_sitter_cuda::LANGUAGE_CUDA;
+//!
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_cuda::language()).expect("Error loading cuda grammar");
+//! parser
+//!     .set_language(&LANGUAGE_CUDA.into())
+//!     .expect("Error loading cuda language");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
-//! [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-//! [language func]: fn.language.html
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
-extern "C" {
-    fn tree_sitter_cuda() -> Language;
+unsafe extern "C" {
+    fn tree_sitter_cuda() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
+/// The tree-sitter [`LanguageFn`] for CUDA.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_cuda() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE_CUDA: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_cuda) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -35,18 +35,20 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // Uncomment these to include any queries that this grammar contains
 
-pub const HIGHLIGHTS_QUERY: &'static str = include_str!("../../queries/highlights.scm");
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 // pub const INJECTIONS_QUERY: &'static str = include_str!("../../queries/injections.scm");
 // pub const LOCALS_QUERY: &'static str = include_str!("../../queries/locals.scm");
 // pub const TAGS_QUERY: &'static str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
+    use crate::LANGUAGE_CUDA;
+
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language())
+            .set_language(&LANGUAGE_CUDA.into())
             .expect("Error loading cuda language");
     }
 }
